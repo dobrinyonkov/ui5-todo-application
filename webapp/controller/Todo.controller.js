5a618ab668cc0cab4228f975af6ca84445aa675b
Elena Stoyanova <elena.stoyanova@sap.com>
2021-09-21 12:52:55 +0300

    Revert "correct step 4"
    This reverts commit 6bf76afee8c61001aa13aed80576bee6887e295c.

--- webapp/controller/Todo.controller.js
+++ webapp/controller/Todo.controller.js
@@ -153,12 +153,14 @@ sap.ui.define([
 						new Icon("__deleteDialogIcon", { src: "sap-icon://delete", size: "18px" }).addStyleClass("sapUiTinyMargin"),
 						new Text("__deleteDialogText", { text: "Do you want to delete this todo?" })
 					],
-					ariaDescribedBy: ["__deleteDialogText"],
+					ariaDescribedBy: ["__dialogIcon", "__dialogText"],
 					beginButton: new Button({
 						icon:"sap-icon://accept",
 						type: sap.m.ButtonType.Accept,
 						press: function () {
 							this.onDelete();
+							this.resetValues();
+
 							this.oDeleteDialog.close();
 
 							MessageToast.show(sMessage);
